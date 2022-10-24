9fb2ad76378911d7aa72b4264881614badba080f
Brandon <highb@users.noreply.github.com>
2022-10-23 17:22:43 -0700

    Add fly.io to GHA

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* POST echo: sends back the same message */
 router.post('/v1/echo', function(req, res, next) {
   let message = req.body.message
 
@@ -13,6 +13,20 @@ router.post('/v1/echo', function(req, res, next) {
     message: req.body.message
   }
   res.send(echo);
+});
+
+/* POST reverse: sends back the message reversed */
+router.post('/v1/reverse', function(req, res, next) {
+  let message = req.body.message
+
+  if (!message) {
+    throw new Error('Request must provide JSON document with message field to be returned.')
+  }
+
+  let reversed = {
+    message: req.body.message.split("").reverse().join("")
+  }
+  res.send(reversed);
 });
 
 module.exports = router;
